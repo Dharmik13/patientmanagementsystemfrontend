1c161a24d0929f22c50756bc5e9487f326e0668f
Dharmik13 <88429448+Dharmik13@users.noreply.github.com>
2022-09-19 12:42:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Patient from './components/Patient';
 import PatientList from './components/PatientList';
 import { Routes, Route} from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">
